6080233f20d96da1607bb66d85837d326ee930d1
Luke Valenty <luke@tinyfpga.com>
2018-02-25 23:10:25 -0800

    more tinyprog updates...working with new bootloader and metadata

--- programmer/tinyprog/__init__.py
+++ programmer/tinyprog/__init__.py
@@ -3,18 +3,57 @@ import time
 import json
 import jsonmerge
 import re
-import intelhex 
+from intelhex import IntelHex
+
+
+
+byte_mirror_table = bytearray([
+    0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0,
+    0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8,
+    0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
+    0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC,
+    0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2,
+    0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+    0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6,
+    0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+    0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+    0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9,
+    0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+    0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+    0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3,
+    0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+    0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
+    0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+])
+
+
+
+def _mirror_byte(byte):
+    return bit_reverse_table[byte]
+
+
+
+def _mirror_each_byte(data):
+    mirrored_data = bytearray(len(data))
+    for i in range(0, len(data)):
+        mirrored_data[i] = _mirror_byte(data[i])
+    return mirrored_data
+
+
 
 class TinyMeta(object):
     def __init__(self, prog):
         self.prog = prog
+        prog.wake()
         self.root = self._read_metadata()
+
 
     def _parse_json(self, data):
         try:
             return json.loads(data)
         except:
             return None
+
 
     def _resolve_pointers(self, meta):
         if isinstance(meta, dict):
@@ -23,97 +62,132 @@ class TinyMeta(object):
         if isinstance(meta, list):
             return [self._resolve_pointers(v) for v in meta]
 
-        if isinstance(meta, str):
-            m = re.search(r"^\s*@\s*0x(?P<addr>[A-Fa-f0-9]+)\s*\+\s*(?P<len>\d+)\s*$")
+        if isinstance(meta, (str, unicode)):
+            m = re.search(r"^\s*@\s*0x(?P<addr>[A-Fa-f0-9]+)\s*\+\s*(?P<len>\d+)\s*$", meta)
             if m:
-                return json.loads(self.prog.read(int(m.groups("addr"), 16), int(m.groups("len"))))
+                data = self.prog.read(int(m.group("addr"), 16), int(m.group("len")))
+                return json.loads(data)
             else:
                 return meta
 
         return meta
 
+
     def _read_metadata(self):
-        meta_roots = [self._parse_json(self.prog.read_security_register_page(p)) for p in [1, 2, 3]]
+        meta_roots = [self._parse_json(self.prog.read_security_register_page(p).replace("\x00", "")) for p in [1, 2, 3]]
         meta_roots = [root for root in meta_roots if root is not None]
-        meta = reduce(jsonmerge.merge, meta_roots)
+        if len(meta_roots) > 0:
+            meta = reduce(jsonmerge.merge, meta_roots)
+            return self._resolve_pointers(meta)
+        return None
 
-        return self._resolve_pointers(meta)
 
+    def bootloader_addr_range(self):
+        return self._get_addr_range(u"bootloader")
 
+    def userimage_addr_range(self):
+        return self._get_addr_range(u"userimage")
+
+    def _get_addr_range(self, name):
+        addr_str = self.root[u"bootmeta"][u"addrmap"][name]
+        m = re.search(r"^\s*0x(?P<start>[A-Fa-f0-9]+)\s*-\s*0x(?P<end>[A-Fa-f0-9]+)\s*$", addr_str)
+        if m:
+            return (int(m.group("start"), 16), int(m.group("end")))
+        else:
+            return None
+
+    def uuid(self):
+        return str(self.root[u"boardmeta"][u"uuid"])
 
 
 class TinyProg(object):
     def __init__(self, ser, progress=None):
         self.ser = ser
+
         if progress is None:
             self.progress = lambda x: x
         else:
             self.progress = progress
 
+        self.meta = TinyMeta(self)
+
+
     def is_bootloader_active(self):
         self.wake()
         devid = self.read_id()
-        print "flash id: " + str([hex(ord(x)) for x in devid])
         if devid not in ['\xff\xff\xff']:
             return True
         return False
 
-    def cmd(self, opcode, addr=None, data='', cmd_read_len=0):
+
+    def cmd(self, opcode, addr=None, data='', read_len=0):
         assert isinstance(data, str)
         addr = '' if addr is None else struct.pack('>I', addr)[1:]
         write_string = chr(opcode) + addr + data
         cmd_write_string = '\x01{}{}'.format(
-            struct.pack('<HH', len(write_string), cmd_read_len),
+            struct.pack('<HH', len(write_string), read_len),
             write_string,
         )
         self.ser.write(cmd_write_string)
         self.ser.flush()
-        return self.ser.read(cmd_read_len)
+        return self.ser.read(read_len)
+
 
     def sleep(self):
         self.cmd(0xb9)
 
+
     def wake(self):
         self.cmd(0xab)
+
 
     def read_id(self):
         return self.cmd(0x9f, read_len=3)
 
+
     def read_sts(self):
         return self.cmd(0x05, read_len=1)
+
 
     def erase_security_register_page(self, page):
         self.write_enable()
         self.cmd(0x44, page << 8)
         self.wait_while_busy()
 
+
     def program_security_register_page(self, page, data):
         self.write_enable()
         self.cmd(0x42, page << 8, data)
         self.wait_while_busy()
 
+
     def read_security_register_page(self, page):
-        return self.cmd(0x48, page << 8, '\x00', read_len=256)
+        return self.cmd(0x48, addr=page << 8, data='\x00', read_len=255)
+
 
     def read(self, addr, length):
         data = ''
         while length > 0:
-            read_length = min(1024, length)
+            read_length = min(255, length)
             data += self.cmd(0x0b, addr, '\x00', read_len=read_length)
             self.progress(read_length)
             addr += read_length
             length -= read_length
         return data
 
+
     def write_enable(self):
         self.cmd(0x06)
+
 
     def write_disable(self):
         self.cmd(0x04)
 
+
     def wait_while_busy(self):
         while ord(self.read_sts()) & 1:
             pass
+
 
     def _erase(self, addr, length):
         opcode = {
@@ -124,6 +198,7 @@ class TinyProg(object):
         self.write_enable()
         self.cmd(opcode, addr)
         self.wait_while_busy()
+
 
     def erase(self, addr, length):
         possible_lengths = (1, 4 * 1024, 32 * 1024, 64 * 1024)
@@ -174,12 +249,14 @@ class TinyProg(object):
             length -= erase_length
             addr += erase_length
 
+
     # don't use this directly, use the public "write" function instead
     def _write(self, addr, data):
         self.write_enable()
         self.cmd(0x02, addr, data)
         self.wait_while_busy()
         self.progress(len(data))
+
 
     def write(self, addr, data):
         offset = 0
@@ -191,6 +268,7 @@ class TinyProg(object):
             self._write(addr, write_data)
             offset += write_length
             addr += write_length
+
 
     def program(self, addr, data):
         self.progress("Erasing designated flash pages")
@@ -209,18 +287,31 @@ class TinyProg(object):
             self.progress("Failure!")
             return False
 
+
     def boot(self):
         self.ser.write("\x00")
         self.ser.flush()
 
+
     def slurp(self, filename):
-        with open(filename, 'rb') as f:
-            bitstream = f.read()
-        if filename.endswith('.hex'):
-            bitstream = ''.join(chr(int(i, 16)) for i in bitstream.split())
-        elif not filename.endswith('.bin') and not filename.endswith('.bit'):
+        if filename.endswith('.bit') or filename.endswith('.bin'):
+            with open(filename, 'rb') as f:
+                return f.read()
+
+        elif filename.endswith('.hex'):
+            with open(filename, 'rb') as f:
+                return ''.join(chr(int(i, 16)) for i in f.read().split())
+
+        elif filename.endswith('.mcs'):
+            ih = IntelHex()
+            ih.fromfile(filename, format='hex')
+            bitstream = ih.tobinstr(start=0, end=ih.maxaddr())
+            bitstream = _mirror_each_byte(bitstream)
+            return bitstream
+
+        else:
             raise ValueError('Unknown bitstream extension')
-        return (0x30000, bitstream)
+
 
     def program_bitstream(self, addr, bitstream):
         self.progress("Waking up SPI flash")
--- programmer/tinyprog/__main__.py
+++ programmer/tinyprog/__main__.py
@@ -3,43 +3,67 @@ def main():
     import argparse
     import serial
     from serial.tools.list_ports import comports
-    from tinyfpgab import TinyFPGAB
+    from tinyprog import TinyProg
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-l", "--list", action="store_true",
-                        help="list connected and active TinyFPGA B-series "
-                             "boards")
+                        help="list connected and active FPGA boards")
     parser.add_argument("-p", "--program", type=str,
-                        help="program TinyFPGA board with the given bitstream")
+                        help="program FPGA board with the given bitstream")
+    parser.add_argument("-u", "--program-userdata", type=str,
+                        help="program FPGA board with the given user data")
     parser.add_argument("-b", "--boot", action="store_true",
-                        help="command the TinyFPGA B-series board to exit the "
+                        help="command the FPGA board to exit the "
                              "bootloader and load the user configuration")
     parser.add_argument("-c", "--com", type=str, help="serial port name")
+    parser.add_argument("-i", "--id", type=str, help="FPGA board ID")
     parser.add_argument("-d", "--device", type=str, default="1209:2100",
-                        help="device id (vendor:product); default is "
-                             "TinyFPGA-B (1209:2100)")
+                        help="device id (vendor:product); default is (1209:2100)")
     parser.add_argument("-a", "--addr", type=int,
                         help="force the address to write the bitstream to")
+    parser.add_argument("-m", "--meta", action="store_true",
+                        help="dump out the metadata for all connected boards in JSON")
 
     args = parser.parse_args()
-
-    print ""
-    print "    TinyFPGA B-series Programmer CLI"
-    print "    --------------------------------"
 
     device = args.device.lower().replace(':', '')
     if len(device) != 8 or not all(c in '0123456789abcdef' for c in device):
         print "    Invalid device id, use format vendor:product"
         sys.exit(1)
     device = '{}:{}'.format(device[:4], device[4:])
-    print "    Using device id {}".format(device)
     active_boards = [p[0] for p in comports() if device in p[2]]
+
+    if args.meta:
+        meta = []
+        for port in active_boards:
+            with serial.Serial(port, timeout=1.0, writeTimeout=1.0) as ser:
+                p = TinyProg(ser)
+                m = p.meta.root
+                m["port"] = port
+                meta.append(m)
+        import json
+        print json.dumps(meta, indent=2)
+        sys.exit(0)
+
+    print ""
+    print "    TinyProg CLI"
+    print "    ------------"
+    
+    print "    Using device id {}".format(device)
 
     # find port to use
     active_port = None
     if args.com is not None:
         active_port = args.com
+    elif args.id is not None:
+        for port in active_boards:
+            with serial.Serial(port, timeout=1.0, writeTimeout=1.0) as ser:
+                p = TinyProg(ser)
+                if p.meta.uuid().startswith(args.id):
+                    active_port = port
+                    break
+
     elif not active_boards:
         print "    No port was specified and no active bootloaders found."
         print "    Activate bootloader by pressing the reset button."
@@ -48,13 +72,20 @@ def main():
         print "    Only one board with active bootloader, using it."
         active_port = active_boards[0]
     else:
-        print "    Please choose a board with the -c option."
+        print "    Please choose a board with the -c or -i option."
 
     # list boards
     if args.list or active_port is None:
         print "    Boards with active bootloaders:"
-        for p in active_boards:
-            print "        " + p
+        for port in active_boards:
+            with serial.Serial(port, timeout=1.0, writeTimeout=1.0) as ser:
+                p = TinyProg(ser)
+                m = p.meta.root
+                print ""
+                print "        %s: %s %s" % (port, m[u"boardmeta"][u"name"], m[u"boardmeta"][u"hver"])
+                print "            UUID: %s" % m[u"boardmeta"][u"uuid"]
+                print "            FPGA: %s" % m[u"boardmeta"][u"fpga"]
+
         if len(active_boards) == 0:
             print "        No active bootloaders found.  Check USB connections"
             print "        and press reset button to activate bootloader."
@@ -67,34 +98,29 @@ def main():
             if isinstance(info, str):
                 print "    " + info
 
-        for attempt in range(3):
-            with serial.Serial(active_port, 115200, timeout=0.2,
-                               writeTimeout=0.2) as ser:
-                fpga = TinyFPGAB(ser, progress)
-                (addr, bitstream) = fpga.slurp(args.program)
-                if args.addr is not None:
-                    addr = args.addr
-                if addr < 0:
-                    print "    Negative write addr: {}".format(addr)
-                    sys.exit(1)
-                #if addr + len(bitstream) >= 0x400000:
-                #    print "    Write addr over 4Mio: {}".format(addr)
-                #    sys.exit(1)
-                if not fpga.is_bootloader_active():
-                    print "    Bootloader not active"
-                    continue
-                print "    Programming at addr {:06x}".format(addr)
-                if fpga.program_bitstream(addr, bitstream):
-                    sys.exit(0)
-                else:
-                    continue
+        with serial.Serial(active_port, timeout=1.0, writeTimeout=1.0) as ser:
+            fpga = TinyProg(ser, progress)
+            bitstream = fpga.slurp(args.program)
+            addr = fpga.meta.userimage_addr_range()[0]
+            if args.addr is not None:
+                addr = args.addr
+            if addr < 0:
+                print "    Negative write addr: {}".format(addr)
+                sys.exit(1)
+            if not fpga.is_bootloader_active():
+                print "    Bootloader not active"
+                sys.exit(1)
+            print "    Programming at addr {:06x}".format(addr)
+            if fpga.program_bitstream(addr, bitstream):
+                sys.exit(0)
+            else:
+                sys.exit(1)
 
     # boot the FPGA
     if args.boot:
         print "    Booting " + active_port
-        with serial.Serial(active_port, 115200, timeout=0.2,
-                           writeTimeout=0.2) as ser:
-            fpga = TinyFPGAB(ser)
+        with serial.Serial(active_port, timeout=1.0, writeTimeout=1.0) as ser:
+            fpga = TinyProg(ser)
             fpga.boot()
 
 if __name__ == '__main__':
