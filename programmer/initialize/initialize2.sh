d2ae64fc1c096c75d2f4762c0afa5a491817c067
Emard <vordah@gmail.com>
2018-07-14 09:30:36 +0200

    remove old initialize2.sh

--- programmer/initialize/initialize2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-# -a n*1024*1024-4*1024
-# this doesn't work
-tinyprog -a $(printf "%d" 0x3FF000) -u boardmeta.bin
-tinyprog -a $(printf "%d" 0x140000) -u ../../boards/ulx3s-v1.7-45f/tinyfpga_45k.bit
-./jump.py > jump.bin
-tinyprog -a $(printf "%d" 0x3FFF00) -u jump.bin
-hexdump -C jump.bin
