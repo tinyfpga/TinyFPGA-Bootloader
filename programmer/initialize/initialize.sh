25e7a81b569ef07c9a0840e668f97da52c2388ee
Emard <vordah@gmail.com>
2018-07-14 09:29:37 +0200

    dual boot ECP5 initialize script which works

--- programmer/initialize/initialize.sh
+++ programmer/initialize/initialize.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
-# -a n*1024*1024-4*1024
-# tinyprog -a 4190208 -u boardmeta.bin
-tinyprog -a 8384512 -u boardmeta.bin
+#!/bin/sh -e
+
+# Initializer for ECP5 dual boot.
+
+# first half of the FLASH is for user bitstram
+# bootloader bitstream starts at second half of the FLASH
+# board metadata at last 0x1000 bytes
+# Jump command at last 0x100 bytes
+
+# flash size:  16 Mbit =  2 MB =  0x200000
+# flash size:  32 Mbit =  4 MB =  0x400000
+# flash size:  64 Mbit =  8 MB =  0x800000
+# flash size: 128 Mbit = 16 MB = 0x1000000
+ 
+golden_image_address=$(printf "%d" 0x200000) # flash size / 2
+board_meta_address=$(printf "%d" 0x3FF000)   # flash size - 0x1000
+jump_command_address=$(printf "%d" 0x3FFF00) # flash size - 0x100
+
+tinyprog -a $golden_image_address -u ../../boards/ulx3s-v1.7-45f/tinyfpga_45k.bit
+tinyprog -a $(printf "%d" $board_meta_address) -u boardmeta.bin
+./jump.py $golden_image_address > jump.bin
+tinyprog -a $(printf "%d" $jump_command_address) -u jump.bin
+# hexdump -C jump.bin
+# check that's board is recognized by tingprog
+tinyprog -l
+#tinyprog -m
