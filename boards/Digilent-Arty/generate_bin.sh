d14f70349ade2c8de864a462c0cb644a0fda326c
Tom Keddie <git@bronwenandtom.com>
2018-10-07 20:51:01 -0700

    Added json template and script to generate image with bootloader and metadata

---
+++ boards/Digilent-Arty/generate_bin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+[ $# -ne 4 ] && echo "usage ${0} bootloader_bin_file json_file output_file" && exit -1
+bootloader_filename=${1}
+json_filename=${2}
+output_filename=${3}
+pad_filename=${1}.pad
+region_size=0x100000
+json_size=0x1000
+file_size=$(stat -c%s ${bootloader_filename})
+echo "File size is ${file_size} bytes"
+pad_size=$((${region_size} - ${file_size} - ${json_size}))
+echo "Generating ${pad_size} bytes of padding"
+pad_size_1k=$((${pad_size} / 1024))
+pad_size=$((${pad_size} - ${pad_size_1k}*1024))
+dd if=/dev/zero of=${pad_filename} bs=1024 count=${pad_size_1k}
+dd if=/dev/zero of=${pad_filename} bs=1 count=${pad_size} oflag=append conv=notrunc
+cat ${bootloader_filename} ${pad_filename} ${json_filename} > ${output_filename}
