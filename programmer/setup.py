4f9064adfd06a3052cc41db7573c3a92e149d173
Tim 'mithro' Ansell <me@mith.ro>
2019-01-18 12:24:13 +1300

    tinyprog: Missing commas on the classifiers.

--- programmer/setup.py
+++ programmer/setup.py
@@ -60,10 +60,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     entry_points={'console_scripts': ['tinyprog = tinyprog.__main__:main']},
 )
