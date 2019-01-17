7d17e7485f2c7332c05ba28d9527a901b5f72109
Tim 'mithro' Ansell <me@mith.ro>
2019-01-17 16:18:52 +1300

    tinyprog: Decode README as utf-8

--- programmer/setup.py
+++ programmer/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode('utf-8')
 
 setup(
     name = 'tinyprog',
