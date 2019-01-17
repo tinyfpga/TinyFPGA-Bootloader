87be847bc74ce44b02a63d64150b686de9e6bedf
Tim 'mithro' Ansell <me@mith.ro>
2019-01-17 15:32:13 +1300

    tinyprog: Upgrade packaging to latest standards.
     * Add README.md to the rendering.
     * Add MANIFEST.ini
     * Add tox.ini for testing in Python versions.

--- programmer/setup.py
+++ programmer/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'tinyprog',
     packages = find_packages(),
@@ -13,17 +17,29 @@ setup(
     ],
     version = '1.0.23',
     description = 'Programmer for FPGA boards using the TinyFPGA USB Bootloader (http://tinyfpga.com)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'Luke Valenty',
     author_email = 'luke@tinyfpga.com',
     url = 'https://github.com/tinyfpga/TinyFPGA-Bootloader/',
-    keywords = ['fpga', 'tinyfpga', 'programmer'],
+    project_urls={
+        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Source': 'https://github.com/tinyfpga/TinyFPGA-Bootloader/tree/master/programmer',
+        'Tracker': 'https://github.com/tinyfpga/TinyFPGA-Bootloader/issues',
+    },
+    license='GPLv3+',
+    keywords = ['fpga', 'tinyfpga', 'programmer', 'lattice', 'ice40'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
     entry_points = {
         'console_scripts': [
