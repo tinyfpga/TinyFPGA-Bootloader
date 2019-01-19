0b706bb568beb1e3750d6710b8795fd15222cb4d
Trammell hudson <hudson@trmm.net>
2019-01-18 21:55:59 -0500

    Tomu: fixes for iverilog tests

--- common/usb_serial_ctrl_ep.v
+++ common/usb_serial_ctrl_ep.v
@@ -23,7 +23,7 @@ module usb_serial_ctrl_ep (
   input in_ep_grant,
   input in_ep_data_free,
   output in_ep_data_put,
-  output reg [7:0] in_ep_data,
+  output [7:0] in_ep_data,
   output in_ep_data_done,
   output reg in_ep_stall,
   input in_ep_acked
